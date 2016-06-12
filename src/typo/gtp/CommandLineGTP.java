151272b7f305794abc4473d999a3151882c4a94c
typohh <typo.hh@gmail.com>
2016-06-12 15:20:38 +0300

    stress tested version

--- src/typo/gtp/CommandLineGTP.java
+++ src/typo/gtp/CommandLineGTP.java
@@ -3,27 +3,45 @@ package typo.gtp;
 import java.io.IOException;
 
 public class CommandLineGTP extends GTP {
-
-	private static final int sLagMargin=3000;
 	
 	@Override
 	public void start( long pTimeInMs ) throws IOException {
 		if( mProcess != null && mProcess.isAlive() ) {
-			mProcess.destroyForcibly();
+			readFromGTP( "quit" );
+			try {
+				Thread.sleep( 50 );
+			} catch( InterruptedException pE ) {
+				throw new Error( pE );
+			}
+			if( mProcess.isAlive() ) {
+				mProcess.destroy();
+			}
+			try {
+				Thread.sleep( 50 );
+			} catch( InterruptedException pE ) {
+				throw new Error( pE );
+			}
+			if( mProcess.isAlive() ) {
+				mProcess.destroyForcibly();
+			}
+			try {
+				Thread.sleep( 50 );
+			} catch( InterruptedException pE ) {
+				throw new Error( pE );
+			}
 		}
 		mProcess = Runtime.getRuntime().exec( mLaunchGTP );
 		setStreams( mProcess.getInputStream() , mProcess.getOutputStream() );
+		mBlacksTurn=true;
 		try {
 			Thread.sleep( 100 );
 		} catch( InterruptedException pE ) {
-			// TODO Auto-generated catch block
-			pE.printStackTrace();
+			throw new Error( pE );
 		}
 		for( String command : mInitializeGTP ) {
 			readFromGTP( command );
 		}
-		int seconds=(int) (( pTimeInMs - sLagMargin ) / 1000);
-		System.out.println( "giving bot " + seconds + " seconds per move." );
+		int seconds=(int) (( pTimeInMs ) / 1000);
 		readFromGTP( "time_settings 0 " + seconds + " 1" );
 	}
 
